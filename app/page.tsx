d9ea877730ccef70430ef2428c232f9e4ef9333c
kbalde <kindi.balde@gmail.com>
2025-05-24 15:27:48 +0200

    r4

--- app/page.tsx
+++ app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Welcome to the webapp
+            Welcome to the webapp to my name is KINDI
           </a>
         </div>
       </main>
